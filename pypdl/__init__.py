4c34ff3334c045b157f535021e4c95b2759049c5
André Bienemann <andre.bienemann@gmail.com>
2019-06-27 21:14:17 +0200

    Modify __init__py

--- pypdl/__init__.py
+++
@@ -1,6 +0,0 @@
-from pypdl.bernoulli import Bernoulli
-from pypdl.binomial import Binomial
-from pypdl.negative_binomial import NegativeBinomial
-
-from pypdl.normal import Normal
-from pypdl.uniform import Uniform
