05208b8e7a7a172f2163df71d6f2f5543e571dda
André Bienemann <andre.bienemann@gmail.com>
2019-07-01 19:00:37 +0200

    Write docstrings for Bernoulli distribution

--- pypdl/bernoulli.py
+++ pypdl/bernoulli.py
@@ -1,17 +1,32 @@
 class Bernoulli:
 
     def __init__(self, p):
+        """
+        This is the discrete probability distribution of
+        a random variable which takes either value 1 or 0 \n
+        :param p: Probability of the positive outcome of the experiment
+        """
         self.p = p
         self.mean = p
         self.var = p * (1 - p)
 
     def pmf(self, x):
+        """
+        Probability mass function \n
+        :param x: Value of the random variable X
+        :return: Probability that X will take a value exactly equal to x
+        """
         if x == 0:
             return 1 - self.p
         else:
             return self.p
 
     def cdf(self, x):
+        """
+        Cumulative distribution function \n
+        :param x: Value of the random variable X
+        :return: Probability that X will take a value less than or equal to x
+        """
         if x == 0:
             return 1 - self.p
         else:
