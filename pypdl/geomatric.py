495a235d1c52b8713792f88976c2755b3a71b9af
André Bienemann <andre.bienemann@gmail.com>
2019-07-02 08:06:04 +0200

    Write docstrings for geometric distribution

--- pypdl/geomatric.py
+++ pypdl/geomatric.py
@@ -1,14 +1,29 @@
 class Geometric:
 
     def __init__(self, p):
+        """
+        This is the discrete probability distributions of the random number X
+        of Bernoulli trials needed to get a single success
+        :param p: Probability of the positive outcome of the experiment
+        """
         self.p = p
         self.mean = 1 / p
         self.var = (1 - p) / (p ** 2)
 
     def pmf(self, x):
+        """
+        Probability mass function \n
+        :param x: Value of the random variable X
+        :return: Probability that X will take a value exactly equal to x
+        """
         return (1 - self.p) ** x * self.p
 
     def cdf(self, x):
+        """
+        Cumulative distribution function \n
+        :param x: Value of the random variable X
+        :return: Probability that X will take a value less than or equal to x
+        """
         if x == 0:
             return self.pmf(0)
         else:
