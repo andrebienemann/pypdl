317803175b01f78e66fdb050419d8e4c99bc803f
André Bienemann <andre.bienemann@gmail.com>
2019-07-30 20:46:13 +0200

    Create template for exponential distribution

---
+++ pypdl/exponential.py
@@ -0,0 +1,29 @@
+class Exponential:
+    """
+    TODO
+    """
+
+    def __init__(self):
+        """
+        TODO
+        """
+        # TODO
+        pass
+
+    def pdf(self, x):
+        """
+        Probability Density Function \n
+        :param x: Value of the random variable X
+        :return: Relative likelihood that the value of the random variable would lie in the sample space
+        """
+        # TODO
+        pass
+
+    def cdf(self, x):
+        """
+        Cumulative Distribution Function \n
+        :param x: value of the random variable X
+        :return: probability that X will take a value less than or equal to x
+        """
+        # TODO
+        pass
